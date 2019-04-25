8a2bb6b2abe44f026c477ff7d3328d0d98e93564
Jacob Albright <jacob.albright23@gmail.com>
2019-04-25 18:18:23 -0500

    lib::write_to_file is working

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,15 @@
-pub fn hello_there() -> String {
-  println!("Oh hello!");
+use std::error::Error;
+use std::io::prelude::*;
+use std::fs::{OpenOptions};
 
-  String::from("")
+pub fn write_to_file(msg: &str, filename: &str) -> Result<(), Box<dyn Error>> {
+  
+  let mut file = OpenOptions::new()
+    .append(true)
+    .create(true)
+    .open(filename)?;
+
+  file.write_all(msg.as_bytes())?;
+
+  Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,15 @@
-use std::{env, process};
-
+use std::env;
 use todo;
 
 fn main() {
 
-    todo::hello_there();
-
     let args: Vec<String> = env::args().collect();
 
-    let arg_string: String = args[1..].iter().fold("".to_string(), |acc, x| {
+    let arg_string = args[1..].iter().fold("".to_string(), |acc, x| {
         acc + " " + x
     });
 
-    println!("{}", arg_string);
-    
-    // for arg in args {
-    //     println!("{}", arg);
-    // }
-
+    if let Err(err) = todo::write_to_file(&arg_string, "todos.txt") {
+        println!("Uh oh!: {}", err);
+    }
 }
