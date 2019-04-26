24bd9cd8eaf67ad637a6a6fd978b3cd3a39d1ae8
Jacob Albright <jacob.albright23@gmail.com>
2019-04-25 19:22:04 -0500

    wrote structs and enums

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,51 @@ use std::error::Error;
 use std::io::prelude::*;
 use std::fs::{OpenOptions};
 
-pub fn write_to_file(msg: &str, filename: &str) -> Result<(), Box<dyn Error>> {
-  
-  let mut file = OpenOptions::new()
-    .append(true)
-    .create(true)
-    .open(filename)?;
+enum Completion {
+  Pending,
+  Completed,
+  Reopened,
+}
 
-  file.write_all(msg.as_bytes())?;
+impl Completion {
+  fn to_string(&self) -> String {
+    match self {
+      Pending => String::from("pending"),
+      Completed => String::from("completed"),
+      Reopened => String::from("reopened"),
+    }
+  }
+}
 
-  Ok(())
+pub struct TodoItem {
+  description: String,
+  status: Completion,
+}
+
+impl TodoItem {
+  pub fn new(description: &str) -> TodoItem {
+    TodoItem {
+      description: String::from(description),
+      status: Completion::Pending,
+    }
+  }
+
+  fn format(&self) -> String {
+    format!("
+description: {}
+status:      {}
+", self.description, self.status.to_string())
+  }
+
+  pub fn save(&self) -> Result<(), Box<dyn Error>> {
+
+    let mut file = OpenOptions::new()
+      .append(true)
+      .create(true)
+      .open("todos.txt")?;
+
+    file.write_all(self.format().as_bytes())?;
+
+    Ok(())
+  }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,15 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let arg_string = args[1..].iter().fold("".to_string(), |acc, x| {
-        acc + " " + x
+    todo::TodoItem::new(&args[1]).save().unwrap_or_else(|err| {
+        panic!("There was an error!: {}", err);
     });
 
-    if let Err(err) = todo::write_to_file(&arg_string, "todos.txt") {
-        println!("Uh oh!: {}", err);
-    }
+    // let arg_string = args[1..].iter().fold("".to_string(), |acc, x| {
+    //     acc + " " + x
+    // });
+
+    // if let Err(err) = todo::write_to_file(&arg_string, "todos.txt") {
+    //     println!("Uh oh!: {}", err);
+    // }
 }
